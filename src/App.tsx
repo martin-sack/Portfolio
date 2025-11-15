88db9b6382a327aa1e0c2190ebf0a0caf2b9f053
Martin Lennyyy <martin.sackey@student.potomac.edu>
2025-11-15 01:29:26 +0000

    Fix: Remove unnecessary routing causing blank page
    - Simplified App.tsx to directly render Home component
    - Removed wouter routing (not needed for single-page portfolio)
    - Portfolio now loads correctly without blank page issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/toaster";
 import Home from "@/pages/Home";
-import NotFound from "@/pages/not-found";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 function App() {
   return (
@@ -22,7 +11,7 @@ function App() {
       <TooltipProvider>
         <ThemeProvider>
           <Toaster />
-          <Router />
+          <Home />
         </ThemeProvider>
       </TooltipProvider>
     </QueryClientProvider>
